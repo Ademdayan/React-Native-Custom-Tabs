c1b478ddd76618e5763c2c7426f780dc3a8dbcd9
Adem Dayan <ademdayan36@gmail.com>
2024-12-20 21:05:45 +0300

    sayfalar oluşturuldu, tab yapısı kuruldu

---
+++ app/_layout.jsx
@@ -0,0 +1,39 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import {Tabs} from 'expo-router'
+const _layout = () => {
+  return (
+    <Tabs>
+        <Tabs.Screen
+            name='index'
+            options={{
+                title: 'Home',
+                
+            }}
+        />
+        <Tabs.Screen
+            name='create'
+            options={{
+                title: 'Create',
+                
+            }}
+        />
+        <Tabs.Screen
+            name='explore'
+            options={{
+                title: 'Explore',
+                
+            }}
+        />
+        <Tabs.Screen
+            name='profile'
+            options={{
+                title: 'Profile',
+                
+            }}
+        />
+    </Tabs>
+  )
+}
+
+export default _layout
